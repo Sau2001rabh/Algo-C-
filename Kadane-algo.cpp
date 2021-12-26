92113a4cb5c92216e623ba85a4a88e432c5c5557
Saurabh Siddharth <82315193+Sau2001rabh@users.noreply.github.com>
2021-12-26 18:20:59 +0530

    Maximum Sub-Array Sum

---
+++ Kadane-algo.cpp
@@ -0,0 +1,51 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long long maxSubarraySum(int arr[], int n){
+        
+        // Your code here
+        long long int cSum=0, maxSum=arr[0];
+        for(long long int i=0; i<n; i++)
+        {
+            cSum+=arr[i];
+            
+            maxSum=(cSum>maxSum ? cSum : maxSum);
+            if(cSum<0)
+                cSum=0;
+            
+        }
+        return maxSum;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t,n;
+    
+    cin>>t; //input testcases
+    while(t--) //while testcases exist
+    {
+        
+        cin>>n; //input size of array
+        
+        int a[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //inputting elements of array
+            
+        Solution ob;
+        
+        cout << ob.maxSubarraySum(a, n) << endl;
+    }
+}
+  // } Driver Code Ends
